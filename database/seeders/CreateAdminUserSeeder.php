8613c2a6cbba24485ef77623113417a4104d909f
Md Anikul Islam Binju <anikul.islam.binju@gmail.com>
2024-10-13 13:35:36 +0600

    up

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class CreateAdminUserSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        $user = User::create([
+            'name' => 'Sorobonno Super Admin',
+            'email' => 'sorobonno@admin.com',
+            'password' => bcrypt('sorobonno@2025')
+        ]);
+        $role = Role::create(['name' => 'Super Admin']);
+        $permissions = Permission::pluck('id','id')->all();
+        $role->syncPermissions($permissions);
+        $user->assignRole([$role->id]);
+    }
+}
