e5588add59f795eb1933247e7fda1cb7adee3439
Md Anikul Islam Binju <anikul.islam.binju@gmail.com>
2025-01-10 21:43:16 +0600

    update on

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,15 +4,10 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite('resources/js/app.js')
-
-
     <!-- favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="{{asset('frontend/favicon_io/apple-touch-icon.png')}}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{asset('frontend/favicon_io/favicon-32x32.png')}}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('frontend/favicon_io/favicon-16x16.png')}}">
-    <link rel="manifest" href="/site.webmanifest">
-    <!-- google fonts -->
-
     <!-- All CSS links Here -->
     <link rel="stylesheet" href="{{asset('frontend/css/all.min.css')}}" />
     <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.min.css')}}" />
