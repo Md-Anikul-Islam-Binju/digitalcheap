16991b29baac7ae33720b77546d993af33b9b681
Md-Anikul-Islam <anikul.islam01@northsouth.edu>
2025-01-13 12:54:01 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -44,9 +44,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 
 Route::get('/', [HomePageController::class, 'frontend'])->name('home');
