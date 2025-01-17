c65478dac847e6396a118b6f36794f0892d736b4
Md Anikul Islam Binju <anikul.islam.binju@gmail.com>
2025-01-17 19:01:13 +0600

    update on

--- resources/js/frontend/Navbar.vue
+++ resources/js/frontend/Navbar.vue
@@ -10,7 +10,7 @@
 
                     <div class="d-flex gap-3 align-items-center">
                         <Link class="cart-icon position-relative d-block d-lg-none text-dark" style="width: 30px;">
-                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">3</span>
+                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">{{totalCartCount}}</span>
                             <svg xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 17h-11v-14h-2" /><path d="M6 5l14 1l-1 7h-13" /></svg>
                         </Link>
                         <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse"
