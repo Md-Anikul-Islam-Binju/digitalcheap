b435a02d51b02290cd4b54c9218df57a3dbe2e8d
Md-Anikul-Islam <anikul.islam01@northsouth.edu>
2025-01-22 13:23:46 +0600

    first commit

--- resources/js/frontend/Navbar.vue
+++ resources/js/frontend/Navbar.vue
@@ -139,9 +139,26 @@ export default {
                             <input type="password" id="password" class="form-control" placeholder="Password" required>
                             <label for="password">Password</label>
                         </div>
+
+
+                        <a href="/forgot-password" class="mb-3">Forgot password?</a>
+
                         <button type="button" id="login-button" class="btn btn-success w-100">Login</button>
+
+                                                <small class="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
+                        <hr class="my-4">
+                        <h2 class="fs-5 fw-bold mb-3">Or use a third-party</h2>
+
+                        <button class="w-100 py-2 mb-2 btn btn-outline-secondary rounded-3" type="submit">
+                            <svg class="bi me-1" width="16" height="16">
+                                <use xlink:href="#github"></use>
+                            </svg>
+                            Sign up with Google
+                        </button>
                     </form>
                 `,
+                showCloseButton: true, // Enables the close button
+                closeButtonHtml: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>',
                 showConfirmButton: false,
                 allowOutsideClick: false,
             });
