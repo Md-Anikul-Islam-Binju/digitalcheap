687e2b5be3da1d986429a83b04fefbf90c5b6a23
Md-Anikul-Islam <anikul.islam01@northsouth.edu>
2025-03-23 10:48:11 +0600

    first commit

--- app/Http/Controllers/TermsConditionController.php
+++ app/Http/Controllers/TermsConditionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\SiteSetting;
+use App\Models\TermsAndCondition;
 use Illuminate\Http\Request;
 
 class TermsConditionController extends Controller
@@ -12,6 +13,7 @@ class TermsConditionController extends Controller
         $siteSettings = SiteSetting::latest()->first();
         $cart = session('cart', []);
         $authUser = auth()->user();
-        return inertia('TermsAndCondition',compact('siteSettings','cart','authUser'));
+        $termsAndCondition = TermsAndCondition::first();
+        return inertia('TermsAndCondition',compact('siteSettings','cart','authUser','termsAndCondition'));
     }
 }
