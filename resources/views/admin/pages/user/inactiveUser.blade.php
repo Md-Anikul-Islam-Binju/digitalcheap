bd6c6437702ef31ec503edc44fae18aa018ea9f9
Md-Anikul-islam-Binju <anikul.islam.binju@gmail.com>
2025-04-20 20:39:30 +0600

    up

--- resources/views/admin/pages/user/inactiveUser.blade.php
+++ resources/views/admin/pages/user/inactiveUser.blade.php
@@ -54,10 +54,8 @@
                                         <li>
                                             <a class="dropdown-item"  style="cursor: pointer;" data-bs-toggle="modal" data-bs-target="#editStatusId{{$usersData->id}}">Change Status</a>
                                         </li>
-                                        <li><a class="dropdown-item" href="#">Assign Tools</a></li>
-                                        <li><a class="dropdown-item" href="#">Order</a></li>
-                                        <li><a class="dropdown-item" href="#">Payment</a></li>
-                                        <li><a class="dropdown-item" href="#">Edit</a></li>
+                                        <li><a class="dropdown-item" href="{{url('chatify',$usersData->id)}}">Chat</a></li>
+                                        <li><a class="dropdown-item" href="{{route('order.manage',$usersData->id)}}">Order</a></li>
                                     </ul>
                                 </div>
                             </td>
